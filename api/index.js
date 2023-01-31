1ad40544a9afb76e7a1ac143f7364a491b5fe8ce
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-02-01 00:28:03 +0800

    api: serve static files (react-dist dir) in production using express server

--- api/index.js
+++ api/index.js
@@ -4,6 +4,23 @@ const app = express();
 
 require('./routes/sampleRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  console.log('NODE_ENV', process.env.NODE_ENV);
+  const path = require('path'); // eslint-disable-line global-require
+
+  // Express will serve up production assets (/client/build/static/ files), e.g. main.js, main.css, etc.
+  // .static is middleware to determine location of the js and css file
+  // express determines the location based on the script and style tags in the index.html
+  // order of operations matters, as express will try to match location of static file first (index.html is outside static dir)
+  app.use(express.static(path.resolve(__dirname, '../client', 'dist')));
+
+  // Express will serve up index.html file if it doesn't recognize the route
+  // catch-all case
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+  });
+}
+
 app.get('/', (req, res) => {
   res.send('<h1>Server running...</h1>');
 });
--- api/routes/sampleRoutes.js
+++ api/routes/sampleRoutes.js
@@ -1,11 +1,11 @@
 const { sampleUser } = require('../models/User');
 
 module.exports = (app) => {
-  app.get('/sample-route', (req, res) => {
+  app.get('/api/sample-route', (req, res) => {
     res.send('<h1>Sample Resource/Page</h1>');
   });
 
-  app.get('/user', (req, res) => {
+  app.get('/api/user', (req, res) => {
     res.send({ email: sampleUser.email, password: sampleUser.password });
   });
 };
