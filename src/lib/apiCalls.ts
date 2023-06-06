fc15669eae1cb866b83f7495d50bcc971702a1b0
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-06-06 23:46:53 +0800

    feat: add registration form and register page

---
+++ src/lib/apiCalls.ts
@@ -0,0 +1,38 @@
+export const fetcher = async ({ url, method, body, json = true }) => {
+  const res = await fetch(url, {
+    method,
+    ...(body && { body: JSON.stringify(body) }),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!res.ok) {
+    // handle your errors
+    throw new Error('API error');
+  }
+
+  if (json) {
+    const data = await res.json();
+    // console.log(data.data);
+    return data.data;
+  }
+};
+
+export const register = (formData) => {
+  const { email, password, confirmPassword } = formData;
+
+  if (password !== confirmPassword) {
+    console.log('Passwords do not match!');
+    return null;
+  } else {
+    const newUser = { email, password };
+
+    return fetcher({
+      url: '/api/auth/register',
+      method: 'post',
+      body: newUser,
+    });
+  }
+};
