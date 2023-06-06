f08a398a41e063a80b262b2894c2e612afd9bda7
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-06-06 23:33:43 +0800

    feat: next-auth authn setup; setup signin api route and form c/o next-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,9 @@
+import type { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+// import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { hash, compare } from 'bcrypt';
+
+import prisma from '@/src/lib/prisma';
 
 export async function hashPassword(password) {
   return await hash(password, 12);
@@ -7,3 +12,135 @@ export async function hashPassword(password) {
 export async function verifyPassword(password, hashedPassword) {
   return await compare(password, hashedPassword);
 }
+
+export const authOptions: NextAuthOptions = {
+  // adapter: PrismaAdapter(prisma as any),
+  // --------------------------------------------
+  // auth storage/handling: session using jwt stored in a cookie
+  // no sessions stored in the DB
+  session: {
+    strategy: 'jwt',
+    maxAge: 60 * 60 * 24,
+    // i think place the fields for the session model here if storing session in db
+  },
+  // --------------------------------------------
+  // auth storage/handling: plain jwt
+  // jwt: { maxAge: 60 * 60 * 24 * 30 },
+  // --------------------------------------------
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    session: async ({ session, token }) => {
+      // console.log('Session Callback', { session, token });
+      session.user = {
+        ...session.user,
+        id: token.id,
+        role: token.role,
+        profile: token.profile,
+      };
+      // alternative shortcut but possible repetition of keys and additional keys included like iat, exp, jti
+      // session.user = {
+      //   ...session.user,
+      //   ...token
+      // };
+      return session;
+      // alternative
+      // return {
+      //   ...session,
+      //   user: {
+      //     ...session.user,
+      //     id: token.id,
+      //     role: token.role,
+      //     profile: token.profile,
+      //   },
+      // };
+    },
+    async jwt({ token, user }) {
+      // console.log("JWT Callback", { token, user });
+      return { ...token, ...user };
+      // alternative
+      // if (user) {
+      //   const u = user as unknown as any;
+      //   return {
+      //     ...token,
+      //     id: u.id,
+      //     role: u.role,
+      //     profile: u.profile,
+      //   };
+      // }
+      // return token;
+    },
+    //   async signIn({ user }) {
+    //     // block signin if necessary
+    //     return true;
+    //   }
+  },
+  providers: [
+    CredentialsProvider({
+      // if using built-in next.js sign-in form
+      id: 'email-pword',
+      name: 'Email & Password',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'text',
+          placeholder: 'your_email@mail.com',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'your password',
+        },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
+
+        const existingUser = await prisma.user.findFirst({
+          where: {
+            email: credentials?.email,
+          },
+          include: {
+            profile: true,
+          },
+        });
+
+        if (!existingUser) {
+          return null;
+        }
+
+        // check if inputted password is equal to existingUser's hashed password in DB
+        const userIsAMatch = await verifyPassword(
+          credentials.password,
+          existingUser.password,
+        );
+
+        if (!userIsAMatch) {
+          return null;
+        }
+
+        // extract required User's fields (excluding password)
+        const { id, email, role, profile: userProfile } = existingUser;
+
+        // only include UserProfile's name fields
+        const { first_name, middle_name, last_name, suffix_name, full_name } =
+          userProfile;
+        const profile = {
+          first_name,
+          middle_name,
+          last_name,
+          suffix_name,
+          full_name,
+        };
+
+        // forward to jwt callback
+        return {
+          id,
+          email,
+          role,
+          profile,
+        };
+      },
+    }),
+  ],
+};
