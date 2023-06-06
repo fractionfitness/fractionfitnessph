8a203407d050de9679ddb53e900b0b8a430019b2
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-06-06 23:28:36 +0800

    chore: add initial api route for User resource

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+
+import prisma from '@/src/lib/prisma';
+
+// Get all Users
+export async function GET(req: Request) {
+  const users = await prisma.user.findMany();
+  // return new Response(JSON.stringify({ data: users }));
+  return NextResponse.json({ data: users });
+}
