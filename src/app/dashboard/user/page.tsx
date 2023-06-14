dcb30c40e5813411101d55c25db560d8a904da19
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-06-14 23:46:27 +0800

    feat: add client-side protected pages from unauthenticated user and redirect after signin; add redirects to middleware to protect page/api routes; test middleware with /dashboard page routes

--- src/app/dashboard/user/page.tsx
+++ src/app/dashboard/user/page.tsx
@@ -1,9 +1,17 @@
 'use client';
 
 import { useSession } from 'next-auth/react';
+import { usePathname, redirect } from 'next/navigation';
 
 export default function UserDashboard() {
-  const { data: session, status } = useSession();
+  const currentUrl = usePathname();
+  const { data: session, status } = useSession({
+    // required: true,
+    // already default config | not needed
+    // onUnauthenticated() {
+    //   redirect(`/api/auth/signin?callbackUrl=${currentUrl}`);
+    // },
+  });
   console.log('status====>', status);
   console.log('session====>', session);
   return <>UserDashboard</>;
