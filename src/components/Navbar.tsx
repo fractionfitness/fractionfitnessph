e405a10ee4cc598f53153910072b04e737cbc46c
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-06-15 20:34:46 +0800

    feat: render Navbar links based on session status

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
+'use client';
+
 import Link from 'next/link';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
+  const currentUrl = usePathname();
+  const { data: session, status } = useSession();
+
   return (
     <nav className={styles.nav}>
       <Link href={'/'}>
@@ -16,15 +23,23 @@ export default function Navbar() {
           <Link href={'/dashboard/user'}>UserDashboard</Link>
         </li>
         {/* ----------------delete this after------------------- */}
-        <li>
-          <Link href={'/api/auth/signin'}>Sign in</Link>
-        </li>
-        <li>
-          <Link href={'/api/auth/signout'}>Sign out</Link>
-        </li>
-        <li>
-          <Link href={'/register'}>Register</Link>
-        </li>
+        {!session && (
+          <>
+            <li>
+              <Link href={`/api/auth/signin?callbackUrl=${currentUrl}`}>
+                Sign in
+              </Link>
+            </li>
+            <li>
+              <Link href={'/register'}>Register</Link>
+            </li>
+          </>
+        )}
+        {session && (
+          <li>
+            <Link href={'/api/auth/signout?callbackUrl=/'}>Sign out</Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
