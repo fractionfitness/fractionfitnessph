10d6a17cc806ff140d200635b2a13f3331770096
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-06-25 22:03:47 +0800

    feat: improve Footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,29 @@
-import Image from 'next/image';
+// import Image from 'next/image';
 
+// import { cn } from '@/lib/utils';
 import styles from './Footer.module.css';
 import LogoAvatar from './LogoAvatar';
+import { Icons } from './Icons';
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
+    <footer className="border-t border-gray-700 bg-gray-900 text-slate-500">
       <ul className={styles.devs}>
-        <li>
+        <li className="items-center space-x-2 flex">
           {/* <Image
             src="/octocat-avatar.png"
             width={45}
             height={45}
             alt="FFPH Logo"
           /> */}
-          <LogoAvatar src="/octocat-avatar.png" alt="@elgato" />
-          <span>El Gato con Botas</span>
+          <span className="font-normal">Developed by:</span>
+          <LogoAvatar
+            src="/octocat-avatar.png"
+            alt="@elgato"
+            className="w-7 h-7"
+            fallbackIcon={<Icons.user className="w-7 h-7" />}
+          />
+          <span className="font-normal">El Gato con Botas</span>
         </li>
       </ul>
     </footer>
