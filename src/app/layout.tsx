948acbf06d2c8ebf20c4a783d71ba87890fc9b80
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-06-25 21:05:33 +0800

    feat: rename Navbar to Header; improve Header styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google';
 import '@/styles/globals.css';
 import { cn } from '@/lib/utils';
 import Providers from '@/components/Providers';
-import Navbar from '@/components/Navbar';
+import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -27,7 +27,7 @@ export default function RootLayout({
         )}
       >
         <Providers>
-          <Navbar />
+          <Header />
           <main className="flex-grow text-center container max-w-7xl mx-auto">
             {children}
           </main>
