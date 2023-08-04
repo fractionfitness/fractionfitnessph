f9164369ca97ac10c3ab08aec7ce627b5f3dfdad
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-08-04 13:36:46 +0800

    feat: add /dashboard/group/:groupId/employees page, display employee data in a table using shadcn-ui components and @tanstack/react-table pkg

---
+++ src/components/DataTable.tsx
@@ -0,0 +1,176 @@
+'use client';
+
+import { useState } from 'react';
+import {
+  ColumnDef,
+  SortingState,
+  ColumnFiltersState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  useReactTable,
+  VisibilityState,
+} from '@tanstack/react-table';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui-shadcn/Table';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from '@/components/ui-shadcn/DropdownMenu';
+import { Icons } from '@/components/Icons';
+import { Input } from '@/components/ui-shadcn/Input';
+import { Button } from '@/components/ui-shadcn/Button';
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+  // added this to get all of the page's groups for the dropdown menu in header: group
+  groups: Array<string | undefined>;
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+  groups,
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+
+  const table = useReactTable({
+    data,
+    columns,
+    // filterFns: {
+    //   filterArrOfGroups
+    // },
+    getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+    },
+  });
+
+  return (
+    <>
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter names..."
+          value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
+          onChange={(event) =>
+            table.getColumn('name')?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm bg-zinc-900 text-gray-50"
+        />
+        {/* this should be a dropdown menu to be populated by the page on the employee's employer (group) | a group can have several descendants */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="outline"
+              className="ml-auto bg-zinc-900 text-gray-50"
+            >
+              Columns
+              <Icons.chevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" className="bg-zinc-900 text-gray-50">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+        {/* <Input
+          placeholder="Filter groups..."
+          value={(table.getColumn('group')?.getFilterValue() as string) ?? ''}
+          onChange={(event) =>
+            table.getColumn('group')?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm bg-zinc-900 text-gray-50"
+        /> */}
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : header.id === 'group'
+                        ? flexRender(header.column.columnDef.header, {
+                            ...header.getContext(),
+                            groups, // queried group and its decendants | prop passed into <DataTable> from /app/dashboard/group/[groupId]/employees page
+                          })
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </>
+  );
+}
