6d87459e25790a3cbbfcd368511d3329e1f559d5
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-08-05 20:48:52 +0800

    refactor: add date obj and string manipulation utilities and config values; move the utility functions in prisma/seedUtils.ts, used by the app, into lib/utils; rename hour to hours to be consistent with Date.getHours() and capitalize all DAY_NAMES to be consistent with db model enums

--- prisma/seedUtils.ts
+++ prisma/seedUtils.ts
@@ -1,20 +1,7 @@
 import { faker } from '@faker-js/faker';
 
 import { TIMEZONE_OFFSET } from '@/config';
-
-const padLeftWithOneZero = (str: String) => {
-  return str.length === 0 ? '' : str.length === 1 ? '0' + str : str;
-};
-
-const padLeftWithTwoZeroes = (str: String) => {
-  return str.length === 0
-    ? ''
-    : str.length === 1
-    ? '00' + str
-    : str.length === 2
-    ? '0' + str
-    : str;
-};
+import { padLeftWithOneZero, padLeftWithTwoZeroes } from '@/lib/utils';
 
 export const generateRandomDateTimeObj = () => {
   // might error if date beyond current is used so set max to 2022
@@ -23,7 +10,7 @@ export const generateRandomDateTimeObj = () => {
   // random day currently doesn't check the session record's day of the week so very likely that the random day will not match
   // prevent february from having a day beyond 28
   const day = faker.helpers.rangeToNumber({ min: 1, max: 28 }).toString();
-  const hour = faker.helpers.rangeToNumber({ min: 0, max: 23 }).toString();
+  const hours = faker.helpers.rangeToNumber({ min: 0, max: 23 }).toString();
   const mins = faker.helpers.rangeToNumber({ min: 0, max: 59 }).toString();
   const secs = faker.helpers.rangeToNumber({ min: 0, max: 59 }).toString();
   const ms = faker.helpers.rangeToNumber({ min: 0, max: 999 }).toString();
@@ -31,22 +18,9 @@ export const generateRandomDateTimeObj = () => {
   const randomMysqlDatetimeString = `${year}-${padLeftWithOneZero(
     month,
   )}-${padLeftWithOneZero(day)}T${padLeftWithOneZero(
-    hour,
+    hours,
   )}:${padLeftWithOneZero(mins)}:${padLeftWithOneZero(
     secs,
   )}.${padLeftWithTwoZeroes(ms)}${TIMEZONE_OFFSET}`;
   return new Date(randomMysqlDatetimeString);
-};
-
-export const convertToMysqlDatetimeString = (dateObj) => {
-  const year = dateObj.getFullYear().toString();
-  // getMonth returns a value of 0-11 for the 12 months
-  const month = `${padLeftWithOneZero((dateObj.getMonth() + 1).toString())}`;
-  const day = `${padLeftWithOneZero(dateObj.getDate().toString())}`;
-  const hour = `${padLeftWithOneZero(dateObj.getHours().toString())}`;
-  const mins = `${padLeftWithOneZero(dateObj.getMinutes().toString())}`;
-  const secs = `${padLeftWithOneZero(dateObj.getSeconds().toString())}`;
-  const ms = `${padLeftWithTwoZeroes(dateObj.getMilliseconds().toString())}`;
-
-  return `${year}-${month}-${day}T${hour}:${mins}:${secs}.${ms}${TIMEZONE_OFFSET}`;
 };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,6 +4,109 @@
 import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+import { DAY_NAMES, TIMEZONE_OFFSET, TWELVE_HOUR_CONVERSION } from '@/config';
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const capitalizeFirstChar = (str) =>
+  str.charAt(0).toUpperCase() + str.slice(1, str.length).toLowerCase();
+
+export const capitalizeAllWords = (words) => {
+  return words
+    .split(' ')
+    .map((word) => capitalizeFirstChar(word))
+    .join(' ');
+};
+
+export const padLeftWithOneZero = (str: String) => {
+  return str.length === 0 ? '' : str.length === 1 ? '0' + str : str;
+};
+
+export const padLeftWithTwoZeroes = (str: String) => {
+  return str.length === 0
+    ? ''
+    : str.length === 1
+    ? '00' + str
+    : str.length === 2
+    ? '0' + str
+    : str;
+};
+
+export const convertToMysqlDatetimeString = (dateObj) => {
+  const year = dateObj.getFullYear().toString();
+  // getMonth returns a value of 0-11 for the 12 months
+  const month = `${padLeftWithOneZero((dateObj.getMonth() + 1).toString())}`;
+  const day = `${padLeftWithOneZero(dateObj.getDate().toString())}`;
+  const hours = `${padLeftWithOneZero(dateObj.getHours().toString())}`;
+  const mins = `${padLeftWithOneZero(dateObj.getMinutes().toString())}`;
+  const secs = `${padLeftWithOneZero(dateObj.getSeconds().toString())}`;
+  const ms = `${padLeftWithTwoZeroes(dateObj.getMilliseconds().toString())}`;
+
+  return `${year}-${month}-${day}T${hours}:${mins}:${secs}.${ms}${TIMEZONE_OFFSET}`;
+};
+
+export const getJsDateObjValues = (dateObj) => {
+  const year = dateObj.getFullYear();
+  const month = dateObj.getMonth();
+  const day = dateObj.getDate();
+  const dayOfWeek = dateObj.getDay();
+  const hours = dateObj.getHours();
+  const mins = dateObj.getMinutes();
+  const secs = dateObj.getSeconds();
+
+  return {
+    year,
+    month,
+    day,
+    dayOfWeek,
+    hours,
+    mins,
+    secs,
+  };
+};
+
+export const convertDateObjValuesToStringValues = (dateObjValues) => {
+  const {
+    year,
+    month,
+    day,
+    dayOfWeek,
+    hours: militaryHours,
+    mins,
+    secs,
+  } = dateObjValues;
+  const { hours, phase } = TWELVE_HOUR_CONVERSION[militaryHours];
+
+  return {
+    year: year.toString(),
+    // JavaScript counts months from 0 to 11
+    month: `${padLeftWithOneZero((month + 1).toString())}`,
+    day: `${padLeftWithOneZero(day.toString())}`,
+    dayOfWeek: DAY_NAMES[dayOfWeek],
+    time: `${padLeftWithOneZero(hours.toString())}:${padLeftWithOneZero(
+      mins.toString(),
+    )}${phase}`,
+    mins: `${padLeftWithOneZero(mins.toString())}`,
+    secs: `${padLeftWithOneZero(secs.toString())}`,
+  };
+};
+
+export const convertTwoDatesToTimeInterval = (
+  dateObjects: Array<string>,
+  // dateObjects: string[],
+) => {
+  const [start, end] = dateObjects.map((item) =>
+    convertDateObjValuesToStringValues(getJsDateObjValues(item)),
+  );
+  return `${start.time} - ${end.time}`;
+};
+
+// gets hours, mins, secs from Date obj and converts that into secs
+// does not give secs since 1970
+// excludes millisecs
+export const getTimeInSecsFromDateObj = ({ hours, mins, secs }) => {
+  // return hours * 60 + mins;
+  return hours * 60 * 60 + mins * 60 + secs;
+};
