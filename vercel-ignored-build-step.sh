f90d0234a737f446e5ddce2b4d3eed78a1adea85
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-08-12 19:11:33 +0800

    cicd: test displaying pull request ids (provided by vercel system environment variables) on git branch: cicd-test

--- vercel-ignored-build-step.sh
+++ vercel-ignored-build-step.sh
@@ -2,7 +2,33 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "dev" ]] ; then
+echo "VERCEL_GIT_PULL_REQUEST_ID: $VERCEL_GIT_PULL_REQUEST_ID"
+
+echo "NEXT_PUBLIC_VERCEL_GIT_PULL_REQUEST_ID: $NEXT_PUBLIC_VERCEL_GIT_PULL_REQUEST_ID"
+
+if [ x"${VERCEL_GIT_PULL_REQUEST_ID}" == "x" ] ; then
+    echo "vercel pr id has no value"
+  else
+    echo "vercel pr id has value"
+fi
+
+if [ ${VERCEL_GIT_PULL_REQUEST_ID:-"unset"} == "unset" ] ; then
+    echo "vercel pr id has no value"
+  else
+    echo "vercel pr id has value"
+fi
+
+if [[ -z "${VERCEL_GIT_PULL_REQUEST_ID}" ]] ; then
+    echo "vercel pr id has no value"
+  else
+    echo "vercel pr id has value"
+fi
+
+# checks git branch
+# if [[ "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "dev" || "$VERCEL_GIT_COMMIT_REF" == "cicd-test" ]] ; then
+
+# checks git branch or PULL_REQUEST_ID on any branch | PRs on main/staging/dev are excluded since their deployments will proceed when commits are pushed cancelling the deployment on PR
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "dev" || x"${VERCEL_GIT_PULL_REQUEST_ID}" != "x" ]] ; then
   # Proceed with the build
     echo "✅ - Build can proceed"
   exit 1;
