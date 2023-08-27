77df658cba8e47d902a7a5802fc74184a074a36e
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-08-27 15:28:59 +0800

    fixup

--- my-scripts/pscale/check-db-changes.sh
+++ my-scripts/pscale/check-db-changes.sh
@@ -5,9 +5,9 @@
 output=$(eval "git diff --name-only --diff-filter=ACMRT $1 $2 | grep -w schema.prisma$" 2>&1)
 exit_status=$?
 
+#  this doesn't run if exit_status != 0
+# echo "exit code: $exit_status"
+
 if [ $exit_status -eq 0 ]; then
   DB_CHANGES=true
-else
-  echo "Error: git diff returned non-zero exit code $exit_status"
-  DB_CHANGES=false
 fi
