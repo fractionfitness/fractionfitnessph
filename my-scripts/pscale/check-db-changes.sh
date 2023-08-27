b32f6e90a28c916a6aae111d5828a1d057ba9662
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-08-27 14:52:34 +0800

    fixup

--- my-scripts/pscale/check-db-changes.sh
+++ my-scripts/pscale/check-db-changes.sh
@@ -2,12 +2,12 @@
 
 # $1 COMMIT_HASH1 $2 COMMIT_HASH2
 
-DB_CHANGES=$(eval "git diff --name-only --diff-filter=ACMRT $1 $2 | grep -w schema.prisma$" 2>&1)
+output=$(eval "git diff --name-only --diff-filter=ACMRT $1 $2 | grep -w schema.prisma$" 2>&1)
 exit_status=$?
 
 if [ $exit_status -eq 0 ]; then
-  export DB_CHANGES=true
+  DB_CHANGES=true
 else
   echo "Error: git diff returned non-zero exit code $exit_status"
-  export DB_CHANGES=false
+  DB_CHANGES=false
 fi
