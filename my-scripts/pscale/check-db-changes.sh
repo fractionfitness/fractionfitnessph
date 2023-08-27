368e4a4d01c186f0c1896df1b46ba46de81aff41
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-08-27 15:46:04 +0800

    fixup

--- my-scripts/pscale/check-db-changes.sh
+++ my-scripts/pscale/check-db-changes.sh
@@ -6,8 +6,10 @@ output=$(eval "git diff --name-only --diff-filter=ACMRT $1 $2 | grep -w schema.p
 exit_status=$?
 
 #  this doesn't run if exit_status != 0
-# echo "exit code: $exit_status"
+echo "exit code: $exit_status"
 
 if [ $exit_status -eq 0 ]; then
   DB_CHANGES=true
+else
+  DB_CHANGES=false
 fi
