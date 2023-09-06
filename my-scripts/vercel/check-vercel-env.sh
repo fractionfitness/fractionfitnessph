513e89a1ff874aa97167776f00917ef27689afa7
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-09-06 21:10:47 +0800

    move branch_name and existing_pword steps to setup job; fix vercel-deploy job's cache step for project deps; add check for vercel env var DATABASE_URL

---
+++ my-scripts/vercel/check-vercel-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# $1 VERCEL_ENV $2 GITHUB_BRANCH_NAME $3 VERCEL_TOKEN $4 VERCEL_ENV_VAR
+
+output=`vercel env ls $1 $2 --token $3 | grep -ow $4 || true 2>&1`
+
+echo "output: ${output}"
+
+if [ "$output" == $4 ]; then
+  EXISTS=true
+else
+  EXISTS=false
+fi
