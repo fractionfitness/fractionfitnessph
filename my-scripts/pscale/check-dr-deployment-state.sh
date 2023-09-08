6ddb06a7f114fe2509ce0772f08769c661cbf96c
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-09-09 02:05:02 +0800

    add workflow to check dr state prior to pr merge

---
+++ my-scripts/pscale/check-dr-deployment-state.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ "$STATE" == "open" && "$APPROVED" == "Yes" && "$DEPLOYMENT_STATE" == "ready" && "$DEPLOYABLE" == "Yes" ]]; then
+  echo "Planetscale Deploy Request is ready for Deployment"
+  exit 0
+else
+  echo "Resolve the following Planetscale Deploy Request Issues:"
+  if [ "$STATE" == "closed" ]; then
+    echo "No Open Deploy Request"
+  fi
+
+  if [ "$APPROVED" != "Yes" ]; then
+    echo "Deploy Request has not yet been Approved"
+  fi
+
+  if [ "$DEPLOYMENT_STATE" != "ready" ]; then
+    echo "Deploy Request state is not equal to \"ready\" or state is equal to \"no_changes\" (meaning no DB schema changes)."
+  fi
+
+  if [ "$DEPLOYABLE" != "Yes" ]; then
+    echo "Deploy Request is not Deployable"
+  fi
+  exit 1
+fi
