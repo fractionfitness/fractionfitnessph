118ee977983cee3be196f6e7255c2f18aae54772
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-09-11 11:49:14 +0800

    distinguish bet existing and created pscale password name and id; move pword cache check to setup job

--- my-scripts/pscale/get-pword-info.sh
+++ my-scripts/pscale/get-pword-info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# GET EXISTING PWORD ID
+# GET PWORD ID
 
 # $1 PSCALE_DB_NAME $2 PSCALE_BRANCH_NAME $3 PSCALE_ORG_NAME $4 PSCALE_PWORD_NAME
 
@@ -8,25 +8,9 @@
 PWORD_ID=`pscale password list $1 $2 --org $3 --format json | jq -r "[.[] | select(.name == \"$4\") ]  | .[0].id "`
 
 if [ "${output}" != null ]; then
-  export EXISTING_PWORD_ID="${PWORD_ID}"
-  export EXISTING_PWORD_NAME=$4
+  export PWORD_ID="${PWORD_ID}"
+  export PWORD_NAME=$4
 else
-  export EXISTING_PWORD_ID=""
-  export EXISTING_PWORD_NAME=""
+  export PWORD_ID=""
+  export PWORD_NAME=""
 fi
-
-echo "EXISTING_PWORD_ID: ${EXISTING_PWORD_ID}"
-echo "EXISTING_PWORD_NAME: ${EXISTING_PWORD_NAME}"
-
-echo "EXISTING_PWORD_ID=${EXISTING_PWORD_ID}" >> $GITHUB_OUTPUT
-echo "EXISTING_PWORD_NAME=${EXISTING_PWORD_NAME}" >> $GITHUB_OUTPUT
-
-# CREATE BRANCH PASSWORD NAME
-# $1 PSCALE_DB_NAME $2 PSCALE_BRANCH_NAME $3 PSCALE_ORG_NAME $4 ROLE
-
-# EXISTING_PWORD_ID=`pscale password list ${{ env.PSCALE_DB_NAME }} ${{ env.PSCALE_BRANCH_NAME }} --org ${{ env.PSCALE_ORG_NAME }} --format json | jq -r "[.[] | select(.name == \"${{ env.PSCALE_PWORD_NAME }}\") ]  | .[0].id "`
-
-# if [ "${4}" != null ]; then
-#   echo "PSCALE_PWORD_NAME: ${PSCALE_BRANCH_NAME}-${ROLE}"
-#   echo "PSCALE_PWORD_NAME=${PSCALE_BRANCH_NAME}-${ROLE}" >> "${GITHUB_OUTPUT}"
-# fi
