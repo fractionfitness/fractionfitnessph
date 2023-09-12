423cdd82c0dc6f019cef7c0e3df0bec9fa0aa395
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-09-12 23:38:23 +0800

    fix check-db-reset.sh to also set RESERT_DB=true, when there's an error and DB_CHANGES=true

--- my-scripts/pscale/check-db-reset.sh
+++ my-scripts/pscale/check-db-reset.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 if [ "$PR_ACTIVITY_TYPE" == "synchronize" ]; then
-  if [[ "$DB_CHANGES_ON_PR_SYNC" == "true" || "$ERROR_DB_CHANGES_ON_PR_SYNC" == "true" ]]; then
+  if [[ "$DB_CHANGES_ON_PR_SYNC" == "true" ]]; then
+    RESET_DB=true
+  # check DB_CHANGES on PR Head vs Base, when there is an error
+  elif [[ "$ERROR_DB_CHANGES_ON_PR_SYNC" == "true" && "$DB_CHANGES" == "true" ]]; then
     RESET_DB=true
   else
     RESET_DB=false
