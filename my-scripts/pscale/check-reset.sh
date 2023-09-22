dd08d037d17ff5aff48c6833237fce5500e5b472
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-09-23 00:30:12 +0800

    adjust workflows to account for PRs from staging into main gh branch

--- my-scripts/pscale/check-reset.sh
+++ my-scripts/pscale/check-reset.sh
@@ -2,7 +2,26 @@
 
 # CHECKS IF EITHER PSCALE DB NEEDS TO BE RESET OR OPEN & APPROVED PSCALE DR NEEDS TO BE RECREATED
 
-if [ "$PR_ACTIVITY_TYPE" == "synchronize" ]; then
+# if STAGING_BRANCH == "true" then RESET=TRUE whether there are db changes or not
+
+# Github branch is staging
+if [ "$STAGING" == "true" ]; then
+  if [ "$PR_ACTIVITY_TYPE" == "synchronize" ]; then
+    if [[ "$DB_CHANGES_ON_PR_SYNC" == "true" ]]; then
+      RESET=true
+    elif [[ "$ERROR_DB_CHANGES_ON_PR_SYNC" == "true" && "$DB_CHANGES" == "true" ]]; then
+      RESET=true
+    else
+      RESET=false
+    fi
+
+  # not a pr sync event
+  else
+    RESET=true
+  fi
+
+# Github branch is not staging and pr sync event
+elif [ "$PR_ACTIVITY_TYPE" == "synchronize" ]; then
   if [[ "$DB_CHANGES_ON_PR_SYNC" == "true" ]]; then
     RESET=true
   # check DB_CHANGES on PR Head vs Base, when there is an error
@@ -11,6 +30,8 @@ if [ "$PR_ACTIVITY_TYPE" == "synchronize" ]; then
   else
     RESET=false
   fi
+
+# Github branch is not staging and not a pr sync event
 else
   if [ "$DB_CHANGES" == "true" ]; then
     RESET=true
