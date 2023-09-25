aec5c6d6b910755ef2310b2fdfa04c6b8a020885
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-09-26 00:36:39 +0800

    add cicd workflows

---
+++ my-scripts/pscale/check-db-changes.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# $1 COMMIT_HASH1 $2 COMMIT_HASH2
+
+# git diff errors:
+# fatal: bad object
+# fatal: ambiguous argument '1023de9033dsadsadsdasd': unknown revision or path not in the working tree
+
+# if git diff and/or grep error, it could be that there was no match (meaning no db changes) or a force push was executed (resulting in a non-existent commit prior to the push)
+
+output=$(eval "git diff --name-only --diff-filter=ACMRT $1 $2 | grep -ow schema.prisma$ || true" 2>&1)
+exit_status=$?
+
+echo "exit code: $exit_status"
+echo "output of git diff: ${output}"
+
+if [ "$output" == "schema.prisma" ]; then
+  DB_CHANGES=true
+  ERROR=false
+else
+  output_error=`echo $output | grep -w "fatal" || true 2>&1`
+  echo "output_error: $output_error"
+  if [ -n "$output_error" ]; then
+    ERROR=true
+    DB_CHANGES=""
+  else
+    ERROR=false
+    DB_CHANGES=false
+  fi
+fi
+
+# echo "DB_CHANGES: $DB_CHANGES"
+# echo "ERROR: $ERROR"
---
+++ my-scripts/pscale/get-pscale-branch-name.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+# GET BRANCH NAME
+# $1 GITHUB_BRANCH_NAME $2 PR_NUMBER
+
+GITHUB_BRANCH_NAME=$1
+GITHUB_PR_NUMBER=$2
+
+# check if GITHUB_BRANCH_NAME has a corresponding Issue Ref Number
+NUMERIC_REGEX='^[0-9]+$'
+SEMVER_REGEX='^[0-9]*\.[0-9]{1,2}\.[0-9]{1,2}'
+
+BRANCH_TYPE=$(echo "${GITHUB_BRANCH_NAME}" | awk -F'[-]' '{print $1}')
+REF_NUM=$(echo "${GITHUB_BRANCH_NAME}" | awk -F'[-]' '{print $2}')
+
+if [ "$BRANCH_TYPE" == "staging" ]; then
+  # it is a staging branch with the naming convention: "staging-semver"
+  echo "$GITHUB_BRANCH_NAME github branch (staging)"
+  if [[ "${REF_NUM}" =~ ${SEMVER_REGEX} ]]; then
+
+  # only gets staging-semver
+    SHORTENED_GH_BRANCH_NAME=$(echo "${GITHUB_BRANCH_NAME}" | awk -F'[-]' '{printf "%s-%s", $1, $2;}')
+
+  else
+    echo "REF_NUM: $REF_NUM"
+
+    echo -e "Error: Github Staging Branch Name must use a valid SemVer Number: XXX.XX.XX. \
+    \nRename the Github Branch, using the following pattern: \
+    \n<staging>-semver_num (e.g. staging-1.0.0-mvp)" 2>&1;
+    exit 1
+  fi
+
+else
+  # it is a feat branch with the naming convention "feat-issueref-description"
+  echo "$GITHUB_BRANCH_NAME github branch (feat/fix/refactor/cicd/chore/etc.)"
+
+  # check if it is a number and not 'noref'
+  # do not enclose NUMERIC_REGEX in "" because it will treat the regex as a string
+  if [[ "${REF_NUM}" =~ ${NUMERIC_REGEX} ]]; then
+    # echo "REF_NUM: $REF_NUM"
+
+    # only gets first_word-issue_num
+    SHORTENED_GH_BRANCH_NAME=$(echo "${GITHUB_BRANCH_NAME}" | awk -F'[-]' '{printf "%s-%s", $1, $2;}')
+
+  else
+    echo "REF_NUM: $REF_NUM"
+
+    echo -e "Error: Github Feature Branch Name must correspond to a Github Issue Reference Number. \
+    \nPlease create an Issue and connect it to your Github Branch. \
+    \nThen, rename the Github Branch, using the following pattern: \
+    \n<feat | fix | refactor | cicd | chore>-issue_num-description (e.g. feat-1-add-dashboard)" 2>&1;
+    exit 1
+  fi
+
+fi
+
+if [ -z "${GITHUB_PR_NUMBER}" ]; then
+  export PSCALE_BRANCH_NAME="${SHORTENED_GH_BRANCH_NAME}"
+else
+  export PSCALE_BRANCH_NAME="${SHORTENED_GH_BRANCH_NAME}-pr-${GITHUB_PR_NUMBER}"
+fi
+
+echo "PSCALE_BRANCH_NAME: ${PSCALE_BRANCH_NAME}"
+echo "SHORTENED_GH_BRANCH_NAME: ${SHORTENED_GH_BRANCH_NAME}"
+echo "SHORTENED_GH_BRANCH_NAME=${SHORTENED_GH_BRANCH_NAME}" >> "${GITHUB_OUTPUT}"
+echo "PSCALE_BRANCH_NAME=${PSCALE_BRANCH_NAME}" >> "${GITHUB_OUTPUT}"
+
+
+
+
+
+
+# # delete password if it already existed
+# # first, list password if it exists
+# local raw_output=`pscale password list "$DB_NAME" "$BRANCH_NAME" --org "$ORG_NAME" --format json `
+# # check return code, if not 0 then error
+# if [ $? -ne 0 ]; then
+#     echo "Error: pscale password list returned non-zero exit code $?: $raw_output"
+#     exit 1
+# fi
+
+# # command output doesn't store the branch password's name in display_name
+# # local output=`echo $raw_output | jq -r "[.[] | select(.display_name == \"$CREDS\") ] | .[0].id "`
+# local output=`echo $raw_output | jq -r "[.[] | select(.name == \"$CREDS\") ] | .[0].id "`
+# # if output is not "null", then password exists, delete it
+# if [ "$output" != "null" ]; then
+#     echo "Deleting existing password $output"
+#     pscale password delete --force "$DB_NAME" "$BRANCH_NAME" "$output" --org "$ORG_NAME"
+#     # check return code, if not 0 then error
+#     if [ $? -ne 0 ]; then
+#         echo "Error: pscale password delete returned non-zero exit code $?"
+#         exit 1
+#     fi
+# fi
+
+# local raw_output=`pscale password create "$DB_NAME" "$BRANCH_NAME" "$CREDS" --org "$ORG_NAME" --format json`
+
+# if [ $? -ne 0 ]; then
+#     echo "Failed to create credentials for database $DB_NAME branch $BRANCH_NAME: $raw_output"
+#     exit 1
+# fi
+
+# local DB_URL=`echo "$raw_output" |  jq -r ". | \"mysql://\" + .username +  \":\" + .plain_text +  \"@\" + .database_branch.access_host_url + \"/\""`
+# local GENERAL_CONNECTION_STRING=`echo "$raw_output" |  jq -r ". | .connection_strings.general"`
+
+# # if not running in CI
+# if [ -z "$CI" ]; then
+#     echo "In the next lines, you will see your secret, branch connection information: "
+#     echo "$SECRET_TEXT"
+#     echo "::set-output name=CONNECTION_STRING_LINK::${link}"
+# fi
+
+# echo "Alternatively, you can connect to your new branch like this:"
+# echo "pscale shell \"$DB_NAME\" \"$BRANCH_NAME\" --org \"$ORG_NAME\""
+# echo "or, to create a local tunnel to the database:"
+# echo "pscale connect \"$DB_NAME\" \"$BRANCH_NAME\" --org \"$ORG_NAME\""
+# export MY_DB_URL=$DB_URL
+
+# # getting DATABASE_URL value from SECRET_TEXT
+# # echo "$SECRET_TEXT" | grep -w "DATABASE_URL" | awk -F' ' '{print $2}'
+
+# # writing to Github Actions Outputs
+# echo "PSCALE_DB_URL=$DB_URL" >> $GITHUB_OUTPUT
