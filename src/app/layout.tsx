4fcdb713d2687dfcbba2bc4ba7b46caf8e389ff5
fractionfitness <123066159+fractionfitness@users.noreply.github.com>
2023-10-04 19:18:04 +0800

    feat: add navbar and footer (#84)
    Co-authored-by: ercsanchez <44195323+ercsanchez@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
-import './globals.css';
 import { Inter } from 'next/font/google';
+
+import './globals.css';
+import Navbar from '@/src/components/Navbar';
+import Footer from '@/src/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
