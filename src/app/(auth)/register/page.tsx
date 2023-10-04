84a9bad06e251daea32df471e925c80bfc8f131d
fractionfitness <123066159+fractionfitness@users.noreply.github.com>
2023-10-04 21:03:30 +0800

    Feat 87 authentication closes #87 (#88)
    * feat: add shared client components
    
    * chore: add initial api route for User resource
    
    * feat: next-auth authn setup; setup signin api route and form c/o next-auth
    
    * feat: add register api route
    
    * feat: add registration form and register page
    
    ---------
    
    Co-authored-by: ercsanchez <44195323+ercsanchez@users.noreply.github.com>

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,5 @@
+import AuthForm from '@/src/components/AuthForm';
+
+export default function Register() {
+  return <AuthForm mode="register" />;
+}
---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from 'next/server';
+
+import prisma from '@/src/lib/prisma';
+import { hashPassword } from '@/src/lib/auth';
+
+export async function POST(req: Request, res: NextResponse) {
+  const { email, password } = await req.json();
+
+  // user input validation
+  if (
+    !email ||
+    !email.includes('@') ||
+    !password
+    // || password.trim().length < 5
+  ) {
+    return NextResponse.json({
+      message: 'Invalid input: email or password',
+      data: null,
+    });
+  }
+
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return NextResponse.json({ message: 'User already exists!', data: null });
+  }
+
+  try {
+    const newUser = await prisma.user.create({
+      data: { email, password: await hashPassword(password) },
+    });
+    const { password, ...newUserExcludePword } = newUser;
+    return NextResponse.json({
+      message: 'Created user!',
+      data: newUserExcludePword,
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
