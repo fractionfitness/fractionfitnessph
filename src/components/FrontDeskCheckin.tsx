f7151f831734eb8c47b18b1bd33520c67804f2a6
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-10-19 22:00:38 +0800

    feat: add employee check ins, guest account creations, group membership inclusions, and guest checkins

--- src/components/FrontDeskCheckin.tsx
+++ src/components/FrontDeskCheckin.tsx
@@ -6,16 +6,29 @@ import {
   useTransition,
   // experimental_useOptimistic,
 } from 'react';
-// import { useParams } from 'next/navigation';
+import { useParams } from 'next/navigation';
 
-import { memberContent } from '@/config';
+import { memberContent, employeeContent, accountContent } from '@/config';
 import { cn } from '@/lib/utils';
-import { addMemberCheckin } from '@/actions/checkin';
-import { searchMemberAction } from '@/actions/member';
+import { addMemberCheckin, addEmployeeCheckin } from '@/actions/checkin';
+import { searchEmployeeAction } from '@/actions/employee';
+import {
+  searchMemberAction,
+  addUserToGroupMembershipAction,
+} from '@/actions/member';
+import { searchUsersAction } from '@/actions/user';
 import { Icons } from '@/components/Icons';
-import SearchSelectUserCommand from './SearchSelectUserCommand';
+import NewAccountForm from '@/components/NewAccountForm';
+import SearchSelectUserCommand from '@/components/SearchSelectUserCommand';
+import SelectGroups from '@/components/SelectGroups';
 import SelectSessions from '@/components/SelectSessions';
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui-shadcn/Accordion';
 import { Button } from '@/components/ui-shadcn/Button';
 import {
   Card,
@@ -27,19 +40,33 @@ import {
 } from '@/components/ui-shadcn/Card';
 import { Input } from '@/components/ui-shadcn/Input';
 import { Label } from '@/components/ui-shadcn/Label';
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '@/components/ui-shadcn/Tabs';
 import { Toggle } from '@/components/ui-shadcn/Toggle';
 
-export default function FrontDeskCheckin({ sessions, employmentGroups }) {
-  const [selectedUser, setSelectedUser] = useState(null);
-  const [selectedSession, setSelectedSession] = useState(null);
+function UserCheckinCard({
+  mode,
+  selectedSessionOrGroup,
+  selectedUser,
+  setSelectedUser,
+  SelectSessionsOrGroups,
+  selectLabel,
+}) {
   const [searchInputValue, setSearchInputValue] = useState('');
   const [pinInputState, setPinInputState] = useState(null);
   const [pinToggleShow, setPinToggleShow] = useState(false);
   const [isPending, startTransition] = useTransition();
   const userPinRef = useRef(null);
 
-  const { userType, groupType, userRoles, userStatus } = memberContent;
+  const { userType, groupType, userRoles, userStatus } =
+    mode === 'member' ? memberContent : employeeContent;
   const userContent = { userType, groupType, userRoles, userStatus };
+  const searchAction =
+    mode === 'member' ? searchMemberAction : searchEmployeeAction;
 
   const handleSubmit = (e) => {
     let pin = userPinRef?.current?.value;
@@ -49,7 +76,13 @@ export default function FrontDeskCheckin({ sessions, employmentGroups }) {
       pin = Number(pin);
     }
 
-    startTransition(() => addMemberCheckin(selectedSession, selectedUser, pin));
+    if (mode === 'member') {
+      startTransition(() =>
+        addMemberCheckin(selectedSessionOrGroup, selectedUser, pin),
+      );
+    } else if (mode === 'employee') {
+      startTransition(() => addEmployeeCheckin(selectedUser, pin));
+    }
 
     setSelectedUser(null);
     setSearchInputValue('');
@@ -65,31 +98,29 @@ export default function FrontDeskCheckin({ sessions, employmentGroups }) {
   };
 
   return (
-    <Card className="w-[400px] bg-background text-foreground">
+    <Card className="bg-background text-foreground">
       <CardHeader>
         <CardTitle>{`${userType}`} Check In</CardTitle>
         <CardDescription>Card description...</CardDescription>
       </CardHeader>
       <CardContent className="space-y-4">
-        <SelectSessions
-          sessions={sessions}
-          setSelectedSession={setSelectedSession}
-          value={selectedSession?.name}
-        />
+        <Label htmlFor="select-session" className="flex ml-1">
+          {selectLabel}
+        </Label>
+        <SelectSessionsOrGroups />
         <SearchSelectUserCommand
           userContent={userContent}
           selectedUser={selectedUser}
-          selectedGroupId={selectedSession?.group_id}
           setSelectedUser={setSelectedUser}
-          searchAction={searchMemberAction}
+          selectedGroupId={
+            mode === 'member'
+              ? selectedSessionOrGroup?.group_id
+              : selectedSessionOrGroup?.id
+          }
+          searchAction={searchAction}
           cmdInputValue={searchInputValue}
           setCmdInputValue={setSearchInputValue}
         />
-        {/* <div className="flex flex-col space-y-1.5">
-          <Label htmlFor="user-pin" className="flex ml-1">
-            {`${userType} PIN:`}
-          </Label> */}
-
         <Label htmlFor="user-pin" className="flex ml-1">
           {`${userType} PIN:`}
         </Label>
@@ -188,7 +219,7 @@ export default function FrontDeskCheckin({ sessions, employmentGroups }) {
         <Button
           onClick={handleSubmit}
           size="lg"
-          disabled={!selectedSession || !selectedUser || !pinInputState}
+          disabled={!selectedSessionOrGroup || !selectedUser || !pinInputState}
           className="w-full"
         >
           {!isPending ? (
@@ -201,5 +232,229 @@ export default function FrontDeskCheckin({ sessions, employmentGroups }) {
       </CardContent>
       <CardFooter>Footer</CardFooter>
     </Card>
+  );
+}
+
+function NewAccountCard({
+  sessions,
+  selectedSession,
+  selectedUser,
+  setSelectedSession,
+  setSelectedUser,
+  employmentGroups,
+}) {
+  const params = useParams();
+  const currentDashboardGroup = employmentGroups.filter(
+    (group) => group.id === +params.groupId,
+  )[0];
+  // if no matching group, then set initial state to null
+  const [selectedGroup, setSelectedGroup] = useState(
+    currentDashboardGroup ? currentDashboardGroup : null,
+  );
+  const [searchInputValue, setSearchInputValue] = useState('');
+
+  const [isPending, startTransition] = useTransition();
+
+  const { userType, groupType, userRoles, userStatus } = accountContent;
+  const userContent = { userType, groupType, userRoles, userStatus };
+
+  return (
+    <Card className="bg-background text-secondary-foreground">
+      <CardHeader>
+        <CardTitle>New Account</CardTitle>
+        <CardDescription>Card Description...</CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <Accordion type="single" collapsible className="w-full text-sm">
+          <AccordionItem value="item-1">
+            <AccordionTrigger>
+              Visitor has existing account & member check in.
+            </AccordionTrigger>
+            <AccordionContent>
+              <div className="space-y-4 p-1">
+                {/* <SearchSelectOneUserCommand
+                  userContent={userContent}
+                  setSelectedUser={setSelectedUser}
+                  selectedGroupId={selectedSession?.group_id}
+                  searchAction={searchUsersAction}
+                /> */}
+                <SearchSelectUserCommand
+                  userContent={userContent}
+                  selectedUser={selectedUser}
+                  setSelectedUser={setSelectedUser}
+                  selectedGroupId={selectedSession?.group_id}
+                  searchAction={searchUsersAction}
+                  cmdInputValue={searchInputValue}
+                  setCmdInputValue={setSearchInputValue}
+                />
+                <SelectSessions
+                  sessions={sessions}
+                  setSelectedSession={setSelectedSession}
+                  value={selectedSession?.name}
+                />
+                {/* ADD ACTION TO ADD VISITOR (HAS A USER ACCOUNT) TO GROUP MEMBERSHIP */}
+                <Button
+                  size="sm"
+                  disabled={!selectedUser || !selectedSession}
+                  // CHECK
+                  onClick={() =>
+                    startTransition(() =>
+                      addUserToGroupMembershipAction(
+                        selectedUser,
+                        selectedGroup.id,
+                        +params.groupId,
+                      ),
+                    )
+                  }
+                >
+                  Add Member & Check In
+                </Button>
+                {/* <Button
+                  variant="destructive"
+                  size="sm"
+                  onClick={(e) => {
+                    setInputValue('');
+                  }}
+                  disabled={!hasValidQuery}
+                >
+                  Clear
+                </Button> */}
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>
+              Create new account & member check in.
+            </AccordionTrigger>
+            <AccordionContent>
+              <NewAccountForm
+                selectedSessionOrGroup={selectedSession}
+                formSubmitBtnLabel={'Sign Up & Member Check In'}
+                selectLabel={`Check in for:`}
+                SelectSessionsOrGroups={() => (
+                  <SelectSessions
+                    sessions={sessions}
+                    setSelectedSession={setSelectedSession}
+                    value={selectedSession?.name}
+                  />
+                )}
+              />
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>
+              Create new account & add as member.
+            </AccordionTrigger>
+            <AccordionContent>
+              <NewAccountForm
+                selectedSessionOrGroup={selectedGroup}
+                formSubmitBtnLabel={'Sign Up & Add Member w/o Check In'}
+                selectLabel={`Add to Group:`}
+                SelectSessionsOrGroups={() => (
+                  <SelectGroups
+                    groups={employmentGroups}
+                    defaultValue={currentDashboardGroup?.name}
+                    selectedGroup={selectedGroup}
+                    handleValueChange={setSelectedGroup}
+                    selectItemsHeader={`Your Group Employers:`}
+                  />
+                )}
+              />
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </CardContent>
+      <CardFooter>Footer</CardFooter>
+    </Card>
+  );
+}
+
+export default function FrontDeskCheckin({ sessions, employmentGroups }) {
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [selectedSession, setSelectedSession] = useState(null);
+
+  const params = useParams();
+  const currentDashboardGroup = employmentGroups.filter(
+    (group) => group.id === +params.groupId,
+  )[0];
+  // if no matching group, then set initial state to null
+  const [selectedGroup, setSelectedGroup] = useState(
+    currentDashboardGroup ? currentDashboardGroup : null,
+  );
+
+  return (
+    <Tabs defaultValue="members" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-3 bg-background text-foreground border">
+        <TabsTrigger
+          value="members"
+          className="data-[state=active]:bg-foreground data-[state=active]:text-background data-[state=active]:shadow-sm"
+        >
+          Members
+        </TabsTrigger>
+        <TabsTrigger
+          value="employees"
+          className="data-[state=active]:bg-foreground data-[state=active]:text-background data-[state=active]:shadow-sm"
+        >
+          Employees
+        </TabsTrigger>
+        <TabsTrigger
+          value="new-account"
+          className="data-[state=active]:bg-foreground data-[state=active]:text-background data-[state=active]:shadow-sm"
+        >
+          New Account
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value="members">
+        <UserCheckinCard
+          mode="member"
+          // sessions={sessions}
+          selectedSessionOrGroup={selectedSession}
+          selectedUser={selectedUser}
+          // setSelectedSessionOrGroup={setSelectedSession}
+          setSelectedUser={setSelectedUser}
+          // value={selectedSession?.name}
+          // SelectSessionsOrGroups={SelectSessions}
+          selectLabel={`Check in for:`}
+          SelectSessionsOrGroups={() => (
+            <SelectSessions
+              sessions={sessions}
+              setSelectedSession={setSelectedSession}
+              value={selectedSession?.name}
+            />
+          )}
+        />
+      </TabsContent>
+      <TabsContent value="employees">
+        <UserCheckinCard
+          mode="employee"
+          // sessions={sessions}
+          selectedSessionOrGroup={selectedGroup}
+          selectedUser={selectedUser}
+          // setSelectedSessionOrGroup={setSelectedGroup}
+          setSelectedUser={setSelectedUser}
+          selectLabel={`Add Account to Group:`}
+          SelectSessionsOrGroups={() => (
+            <SelectGroups
+              groups={employmentGroups}
+              defaultValue={currentDashboardGroup?.name}
+              selectedGroup={selectedGroup}
+              handleValueChange={setSelectedGroup}
+              selectItemsHeader={`Your Group Employers:`}
+            />
+          )}
+        />
+      </TabsContent>
+      <TabsContent value="new-account">
+        {/* change member mode to guest??? */}
+        <NewAccountCard
+          sessions={sessions}
+          selectedSession={selectedSession}
+          selectedUser={selectedUser}
+          setSelectedSession={setSelectedSession}
+          setSelectedUser={setSelectedUser}
+          employmentGroups={employmentGroups}
+        />
+      </TabsContent>
+    </Tabs>
   );
 }
